dc6eddb1c05ff0d8ebb0960d0dc45fab85903908
Nicholas White <nicholaswhite@Nicholass-MacBook-Pro.local>
2017-12-04 16:09:25 +0400

    Created the trigger zone, moving with joystick, forest, castle, and dialog boxes

--- final-project/mover.js
+++ final-project/mover.js
@@ -5,22 +5,35 @@
 
 class Mover {
   constructor() {
-    this.mass = 1;
-    this.position = createVector(30, 30);
+    this.running = [run1,run2,run3,run2,run3,run2];
+    this.position = createVector(30, 300);
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
+    this.x = 0;
   }
 
-
-  display() {
+  //Feed display image to show character running
+  display(dis_img) {
     stroke(0);
     strokeWeight(2);
     fill(255, 0, 0);
-    ellipse(this.position.x,this.position.y,10,10);
+    noStroke();
+    imageMode(CENTER);
+    //Kirby:
+    // idle : 0,0,73,20
+    // running
+    image(dis_img, this.position.x,this.position.y-20,60,60);
+    //ellipse(this.position.x,this.position.y,10,10);
   }
 
+  //When moving change the character's sprite to show moving animation
   move(movement) {
     this.position.add(movement);
+    this.display(this.running[this.x]);
+    this.x++;
+    if(this.x>5){
+      this.x = 0;
+    }
   }
 
 }
--- final-project/sketch.js
+++ final-project/sketch.js
@@ -1,5 +1,42 @@
+//Main World -- Decoding Nature Fall 2017
+//              Zane Mountcastle & Nick White
+
+//var to see number of trees made in the scroller to represent the Forest for Xiu Ai's world
+var numberOfTrees;
+var tb1 = false;
+var tb2 = false;
+
+var serial;          // variable to hold an instance of the serialport library
+var inData;
+var portName = '/dev/cu.usbmodem1411';
+// get the list of ports:
+function printList(portList) {
+ // portList is an array of serial port names
+ for (var i = 0; i < portList.length; i++) {
+ // Display the list the console:
+ print(i + " " + portList[i]);
+ }
+}
+
 function setup()
 {
+  serial = new p5.SerialPort();       // make a new instance of the serialport library
+  serial.on('list', printList);  // set a callback function for the serialport list event
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen);        // callback for the port opening
+  serial.on('data', serialEvent);     // callback for when new data arrives
+  serial.on('error', serialError);    // callback for errors
+  serial.on('close', portClose);      // callback for the port closing
+  var options = { baudrate: 115200};
+  serial.list();                      // list the serial ports
+  serial.open(portName,options);              // open a serial port
+  numberOfTrees = 10;
+  //loading idle Kirby & running Kirby images
+  character = loadImage("media/idleK.png");
+  run1 = loadImage("media/run1K.png");
+  run2 = loadImage("media/run2K.png");
+  run3 = loadImage("media/run3K.png");
+  tree = loadImage("media/tree.png");
   createCanvas(3000,700);
   m = new Mover();
 
@@ -7,21 +44,41 @@ function setup()
 
 function draw()
 {
-  background(0);
-  fill(255,0,0);
-  m.display();
+  console.log("SD: " + inData);
+  background(255);
+  fill(0);
+  //ground of world
+  rect(0,300,3000,500);
 
-  if (m.position.x > width/2){
-    window.scrollTo(m.position.x - width/2, 0);
+  createCastle();
+  createForest();
+  checkTriggerZone();
+
+
+
+
+  if (m.position.x > screen.width/2){
+    window.scrollTo(m.position.x-screen.width/2, 0);
   }
 
-  if (keyIsDown('39')){ // Right
+  if (inData > 50){ // Right
     m.move(5);
   }
-
-  if (keyIsDown('37')) { // Left
+  else if (inData < 50) { // Left
     m.move(-5);
   }
+  else{
+    m.display(character);
+  }
+  if(tb1 || tb2){
+    fill(255);
+    rect(m.position.x-170,m.position.y+20,330,130);
+    fill(0);
+    rect(m.position.x-160,m.position.y+30,310,110);
+    fill(255);
+    rect(m.position.x-150,m.position.y+40,290,90);
+  }
+
 }
 
 window.addEventListener("keydown", function(e) {
@@ -30,3 +87,123 @@ window.addEventListener("keydown", function(e) {
       e.preventDefault();
   }
 }, false);
+
+
+//creating the trees for the forest
+function createForest(){
+
+  for(var i=0;i<numberOfTrees;i++){
+    image(tree,2500+i*25,250,100,100);
+  }
+
+}
+
+//creating the trees for the forest
+function createCastle(){
+
+  rect(1050,100,30,30);
+  rect(1090,100,30,30);
+  rect(1130,100,30,30);
+  for(var i=0;i<30*7;i+=30){
+    rect(1060,110+i,30,30);
+    rect(1090,110+i,30,30);
+    rect(1120,110+i,30,30);
+  }
+
+  for(var j=0;j<30*7;j+=30){
+    rect(1150+j,160,30,30);
+    rect(1150+j,180,30,30);
+    rect(1150+j,200,30,30);
+    if(j!=4*30 && j!=3*30 && j!=2*30){
+      rect(1150+j,230,30,30);
+      rect(1150+j,260,30,30);
+      rect(1150+j,290,30,30);
+    }
+
+    // rect(150+j,200+j,30,30);
+  }
+
+  rect(1350,100,30,30);
+  rect(1390,100,30,30);
+  rect(1430,100,30,30);
+  for(var i=0;i<30*7;i+=30){
+    rect(1360,110+i,30,30);
+    rect(1390,110+i,30,30);
+    rect(1420,110+i,30,30);
+  }
+
+
+
+
+}
+
+//Function to see if character is before any level starts to jump to other's worlds
+function checkTriggerZone(){
+  //before forest is: x = 2375 y = 200
+  if (m.position.x > 2375 && m.position.x < 2475 && m.position.y < 310){
+    tb1 = true;
+    console.log("XiuAiWorld.run()");
+    //To push character past trigger zone when level is over
+    // m.position.x += 200;
+  }
+  else if (m.position.x > 1210 && m.position.x < 1300 && m.position.y < 310){
+    tb2 = true;
+    console.log("RobertWorld.run()");
+    //To push character past trigger zone when level is over
+    // m.position.x += 200;
+  }
+  else{
+    tb2 = false;
+    tb1 = false;
+  }
+
+}
+
+//Function to have the textbox appear
+function textbox(){
+  //before forest is: x = 2375 y = 200
+
+
+}
+
+function serverConnected() {
+  print('connected to server.');
+}
+
+function portOpen() {
+  print('the serial port opened.')
+}
+
+function serialEvent() {
+  inData = Number(serial.read());
+  // console.log("from serial event: "+ inData)
+}
+
+function serialError(err) {
+  print('Something went wrong with the serial port. ' + err);
+}
+
+function portClose() {
+  print('The serial port closed.');
+}
+
+//For Serial to p5
+//https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-input-to-the-p5-js-ide/
+
+
+//
+// function mousePressed(){
+//   console.log(m.position.x)
+//   console.log(m.position.y)
+// }
+
+
+// Can also be included with a regular script tag
+
+
+// var options = {
+//   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+//   typeSpeed: 40
+// }
+//
+// var typed = new Typed(".element", options);
